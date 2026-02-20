e63d738a1cc4ac8080f3a9375ec8df4f5b191019
juniorbonini <juniorboninia7x@gmail.com>
2026-02-19 21:08:49 -0300

    chore: adicionar novas dependências ao projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { LayoutInterface } from "../interfaces/Layout/layout-interface";
+import { LayoutInterface } from "@/interfaces/Layout/layout-interface";
 
 export const metadata: Metadata = {
   title: "Criado po Junior Bonini com Next.js",
